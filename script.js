8a2f4eb3bbc0352c720f2bceea728f9202554d75
Alan <alanrejnowicz281@gmail.com>
2020-04-30 19:32:28 +0200

    Created functions that make it so that the correct equations are calculated first (precedence) and correct the behaviour of the minus sign (minusBehaviour)

--- script.js
+++ script.js
@@ -5,6 +5,35 @@ const allOperators = document.querySelectorAll(".operator");
 
 let operation = [];
 
+function precedence() {
+  for (let i = 1; i < operation.length; i += 2) { // every operator
+    switch (operation[i]) {
+      case "*":
+        operation.splice([i] - 1, 3, operation[i - 1] * operation[i + 1]);
+        break;
+      case "/":
+        operation.splice([i] - 1, 3, operation[i - 1] / operation[i + 1]);
+        break;
+      case "%":
+        operation.splice([i] - 1, 3, operation[i - 1] % operation[i + 1]);
+        break;
+    }
+  }
+}
+
+function minusBehaviour() {
+  for (let i = 0; i < operation.length; i += 2) { // every number
+    if (operation[i - 1] === "-") {
+      operation.splice([i] - 1, 2, "+", -operation[i]);
+    }
+  }
+}
+
+function calcLogic() {
+  minusBehaviour();
+  precedence();
+}
+
 for (btn of numberButtons) {
   btn.addEventListener("click", function () {
     if (screenSection.textContent === "0" || screenSection.textContent === "00") { // on click, the starting 0 will dissapear
@@ -19,6 +48,7 @@ for (btn of numberButtons) {
 for (operator of allOperators) {
   operator.addEventListener("click", function () {
     operation.push(+screenSection.textContent);
+    calcLogic();
 
     screenSection.textContent = "";
     operation.push(this.textContent);
